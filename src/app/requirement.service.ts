ec4a09f3dbb4671e6677b7344de3a5e41abafe5a
Paul Strickland <p.e.strickland@gmail.com>
2018-08-27 22:28:20 +0100

    Add HttpClient.get method to getRequirement. Add error handling.

--- src/app/requirement.service.ts
+++ src/app/requirement.service.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 
 // export interface Item { name: string; }
@@ -30,14 +31,34 @@ export class RequirementService {
     this.messageService.add('Requirements fetched.');
     // return of(REQUIREMENTS);
     return this.http.get<Requirement[]>(this.requirementsUrl)
+      .pipe(
+        tap(requirements => this.log('Fetched requirements')),
+        catchError(this.handleError('getRequirements', []))
+      );
   }
 
+  // GET requirement by id. Will 404 if id not found.
   getRequirement(id: number): Observable<Requirement> {
-    this.messageService.add(`Requirement Service fetched id=${id}`);
-    return of(REQUIREMENTS.find(requirement => requirement.id === id));
+    const url = `${this.requirementsUrl}/${id}`;
+    return this.http.get<Requirement>(url).pipe(
+      tap(_ => this.log(`Fetched Requirement id=${id}`)),
+      catchError(this.handleError<Requirement>(`getRequirement id=${id}`))
+      );
   }
 
   private log(message: string) {
     this.messageService.add(`RequirementService: ${message}`);
+  }
+
+  // Handle Http operation that failed.
+  // Let the app continue.
+  // @param operation - name of the operation that failed
+  // @param result - optional value to return as the observable result
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);  // Let the app keep running by returning an empty result.
+    };
   }
 }
