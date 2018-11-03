9c06cfc875db1edda5963b56c3c294e524afe068
Paul Strickland <p.e.strickland@gmail.com>
2018-11-03 18:51:31 +0000

    Refactor auth service. Breaks app.

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,31 +1,91 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
+import { Observable, of } from 'rxjs';
+import { switchMap, first } from 'rxjs/operators';
+import { auth } from 'firebase/app';
+import { AngularFireStorageReference } from '@angular/fire/storage';
+import { Router } from '@angular/router';
+
+interface User {
+  uid: string;
+  email: string;
+  photoURL?: string;
+  displayName?: string;
+  demoAttr?: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  authState: any = null;
+  // authState: any = null;
+  user$: Observable<any>;
 
-  constructor(public afAuth: AngularFireAuth) {
-    this.afAuth.authState.subscribe(data => this.authState = data);
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    this.user$ = this.afAuth.authState.pipe(
+      switchMap(user => {
+        if (user) {
+          return this.afs.doc<any>(`users/${user.uid}`).valueChanges();
+        } else {
+          return of(null);
+        }
+      })
+    );
+  }
+  // this.afAuth.authState.subscribe(data => this.authState = data);
 
+  getUser() {
+    return this.user$.pipe(first()).toPromise();
   }
 
-  get authenticated(): boolean {
-    return this.authState !== null;
+  googleSignIn() {
+    const provider = new auth.GoogleAuthProvider();
+    return this.oAuthLogin(provider);
   }
 
-  get currentUserId(): string {
-    return this.authenticated ? this.authState.uid : null;
-  }
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  private async oAuthLogin(provider) {
+    const credential = await this.afAuth.auth.signInWithPopup(provider);
+    return this.updateUserData(credential.user);
   }
 
-  logout() {
-    this.afAuth.auth.signOut();
+  private updateUserData({ uid, email, displayName, photoURL }) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${uid}`);
+
+    const data = {
+      uid,
+      email,
+      displayName,
+      photoURL
+    };
+
+    return userRef.set(data, { merge: true });
+  }
+
+  async signOut() {
+    await this.afAuth.auth.signOut();
+    return this.router.navigate(['/']);
   }
 }
+
+//   get authenticated(): boolean {
+//     return this.authState !== null;
+//   }
+
+//   get currentUserId(): string {
+//     return this.authenticated ? this.authState.uid : null;
+//   }
+//   login() {
+//     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+//   }
+
+//   logout() {
+//     this.afAuth.auth.signOut();
+//   }
+// }
