1f130d33b8144df4081a77223cd0c3486b0677a1
Paul Strickland <p.e.strickland@gmail.com>
2018-11-04 22:24:42 +0000

    List projects on dashboard.

--- src/app/requirements/dashboard/dashboard.component.ts
+++ src/app/requirements/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AuthService } from 'src/app/core/auth.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  projectsCollection: AngularFirestoreCollection<any>;
+  projects$: Observable<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private afs: AngularFirestore,
+    private auth: AuthService) {
+    // Get list of projects associated with user.
+    this.projectsCollection = this.afs.collection('projects');
   }
 
+  ngOnInit() {
+    this.projects$ = this.getProjects();
+    console.log(this.projects$)
+  }
+
+  getProjects() {
+    return this.projectsCollection.snapshotChanges()
+      .pipe(map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      }));
+  }
 }
