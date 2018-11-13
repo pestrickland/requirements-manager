11601342d3cef48f349b4f8355a7499532a7e61e
Paul Strickland <p.e.strickland@gmail.com>
2018-11-13 22:44:31 +0000

    fix: Declare dashboard component within core module.
    Temporary fix whilst major restructure and refactoring continues.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 import { AuthService } from './auth.service';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    SharedModule
   ],
-  declarations: [],
+  declarations: [DashboardComponent],
   providers: [AuthService],
 })
 export class CoreModule { }
